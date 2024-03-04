90100c8253a4fd2f0a9940859e43b2d84912bbaf
Barti <bartosz.majkowski@wp.pl>
2024-03-04 22:31:50 +0100

    added basic validation

--- models/post_profile_model.go
+++ models/post_profile_model.go
@@ -1,7 +1,7 @@
 package models
 
 type PostProfile struct {
-	UserName     string `json:"user_name" bson:"user_name" validate:"required"`
+	UserName     string `json:"user_name" bson:"user_name" validate:"required, max=30"`
 	Email        string `json:"email" bson:"email" validate:"required,email"`
-	ProfileImage string `json:"profile_image" bson:"profile_image" validate:"required"`
+	ProfileImage string `json:"profile_image" bson:"profile_image" validate:"required, uri"`
 }
--- models/profile_model.go
+++ models/profile_model.go
@@ -1,8 +1,8 @@
 package models
 
 type Profile struct {
-	Id           string `json:"_id" bson:"_id" validate:"required"`
-	UserName     string `json:"user_name" bson:"user_name" validate:"required"`
+	Id           string `json:"id" bson:"_id" validate:"required"`
+	UserName     string `json:"user_name" bson:"user_name" validate:"required, max=30"`
 	Email        string `json:"email" bson:"email" validate:"required,email"`
-	ProfileImage string `json:"profile_image" bson:"profile_image" validate:"required"`
+	ProfileImage string `json:"profile_image" bson:"profile_image" validate:"required, uri"`
 }
