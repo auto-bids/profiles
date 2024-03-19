38a4ac3447bb78f0d084610750d439eec7ae9002
Barti <bartosz.majkowski@wp.pl>
2024-03-19 20:20:31 +0100

    models adjustment

--- models/edit_profile_model.go
+++ models/edit_profile_model.go
@@ -1,6 +1,6 @@
 package models
 
 type EditProfile struct {
-	UserName     string `json:"user_name" bson:"user_name" validate:"required, max=30"`
-	ProfileImage string `json:"profile_image" bson:"profile_image" validate:"required, uri"`
+	UserName     string `json:"user_name" bson:"user_name" validate:"required,max=30"`
+	ProfileImage string `json:"profile_image" bson:"profile_image" validate:"required,uri"`
 }
--- models/profile_model.go
+++ models/profile_model.go
@@ -2,7 +2,7 @@ package models
 
 type Profile struct {
 	Id           string `json:"id" bson:"_id" validate:"required"`
-	UserName     string `json:"user_name" bson:"user_name" validate:"required, max=30"`
+	UserName     string `json:"user_name" bson:"user_name" validate:"required,max=30"`
 	Email        string `json:"email" bson:"email" validate:"required,email"`
-	ProfileImage string `json:"profile_image" bson:"profile_image" validate:"required, uri"`
+	ProfileImage string `json:"profile_image" bson:"profile_image" validate:"required,uri"`
 }
